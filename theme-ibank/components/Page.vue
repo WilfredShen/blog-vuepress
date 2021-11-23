29503dccadf3b9990e5d3db72204037fe6932e4a
WilfredShen <signup@wilfredshen.cn>
2021-11-23 16:21:28 +0800

    bug fix

--- theme-ibank/components/Page.vue
+++ theme-ibank/components/Page.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount, watch } from "vue";
+import { onBeforeMount, onMounted, onBeforeUnmount, watch } from "vue";
 import { onBeforeRouteLeave, useRoute } from "vue-router";
 import $ from "jquery";
 import MainLayout from "./MainLayout.vue";
@@ -19,21 +19,21 @@ import Sidebar from "./SideBar.vue";
 
 const route = useRoute();
 
-const scrollToPos = (top: number) => $([document.documentElement, document.body]).animate({ scrollTop: top - 4.6 * 16 }, 400) && void 0;
+onBeforeMount(() => {
+  const scrollToPos = (top: number) => $([document.documentElement, document.body]).animate({ scrollTop: top - 4.6 * 16 }, 400) && void 0;
+  const scrollToEl = (el: any) => {
+    const hash = decodeURIComponent(el.target.hash);
+    const offset = $(hash).offset();
+    offset && scrollToPos(offset.top);
+  };
+  const addListener = () => $("a[href^='#'][href!='#']").on("click", scrollToEl) && void 0;
+  const removeListener = () => $("a[href^='#'][href!='#']").on("click", scrollToEl) && void 0;
 
-const scrollToEl = (el: any) => {
-  const hash = decodeURIComponent(el.target.hash);
-  const offset = $(hash).offset();
-  offset && scrollToPos(offset.top);
-};
-
-const addListener = () => $("a[href^='#'][href!='#']").on("click", scrollToEl) && void 0;
-const removeListener = () => $("a[href^='#'][href!='#']").on("click", scrollToEl) && void 0;
-
-onMounted(addListener);
-watch(() => route.path, addListener);
-onBeforeRouteLeave(removeListener);
-onBeforeUnmount(removeListener);
+  onMounted(addListener);
+  watch(() => route.path, addListener);
+  onBeforeRouteLeave(removeListener);
+  onBeforeUnmount(removeListener);
+});
 </script>
 <style lang="scss">
 .content-wrapper > :not(.article-info) {
